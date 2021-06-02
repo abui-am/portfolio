82b4a05b9a0bd1544711305ad9fd2b625acb3153
Abui <adjiem31@gmail.com>
2021-06-02 22:29:58 +0700

    feat: add papper outline option

--- src/components/container/Paper.tsx
+++ src/components/container/Paper.tsx
@@ -1,12 +1,20 @@
-import styles from 'Paper.module.scss';
 import React from 'react';
+
+import styles from './Paper.module.scss';
 
 export interface PaperProps {
   noShadow?: boolean;
+  withOutline?: boolean;
 }
-const Paper: React.FC<PaperProps> = ({ noShadow, children }) => {
+const Paper: React.FC<PaperProps> = ({ noShadow, withOutline, children }) => {
   return (
-    <div className={styles.paper} style={{ boxShadow: noShadow ? '0' : 'none' }}>
+    <div
+      className={styles.paper}
+      style={{
+        boxShadow: noShadow ? '0px' : '',
+        border: `1px solid ${withOutline ? 'var(--background-tertiary)' : 'transparent'}`,
+      }}
+    >
       {children}
     </div>
   );

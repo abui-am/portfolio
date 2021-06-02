0ee88ac4a684fe4d03642032accc3840cfd1bf95
Abui <adjiem31@gmail.com>
2021-06-02 22:24:41 +0700

    feat: Pill Button

--- src/components/button/PillButton.tsx
+++ src/components/button/PillButton.tsx
@@ -1,27 +1,54 @@
-import { ButtonHTMLAttributes, useState } from 'react';
+import { ButtonHTMLAttributes, useEffect, useState } from 'react';
 
 import { Modify } from '@/typings/typeHelper';
 
 import Button from './Button';
 
 export type PillButtonProps<T> = {
-  buttonData: Modify<
-    ButtonHTMLAttributes<T>,
-    {
-      text: string;
-    }
-  >[];
+  onClick: (val: button<T>) => void;
+  buttonData: button<T>[];
 };
 
-const PillButton: React.FC<PillButtonProps<any>> = ({ buttonData }) => {
+export type button<T> = Modify<
+  ButtonHTMLAttributes<T>,
+  {
+    text: string;
+    value: string;
+  }
+>;
+
+const PillButton: React.FC<PillButtonProps<unknown>> = ({ buttonData, onClick = () => '' }) => {
   const [isActive, setIsActive] = useState(0);
+  const getBorder = (index: number, max: number) => {
+    switch (index) {
+      case 0:
+        return '4px 0px 0px 4px';
+      case max:
+        return '0px 4px 4px 0px';
+      default:
+        return '0px';
+    }
+  };
+
+  useEffect(() => {
+    onClick(buttonData[isActive]);
+  }, [buttonData, isActive, onClick]);
+
   return (
     <div style={{ display: 'flex' }}>
-      {buttonData.map((data, index) => (
-        <Button onClick={() => setIsActive(index)} variant={index === isActive ? 'primary' : 'secondary'}>
-          {data.text}
-        </Button>
-      ))}
+      {buttonData.map((data, index) => {
+        const borderRadius = getBorder(index, buttonData.length - 1);
+        return (
+          <Button
+            key={data.text}
+            onClick={() => setIsActive(index)}
+            style={{ borderRadius }}
+            variant={index === isActive ? 'primary' : 'secondary'}
+          >
+            {data.text}
+          </Button>
+        );
+      })}
     </div>
   );
 };
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,3 +1,3 @@
 import Button from './Button';
-
-export { Button };
+import PillButton from './PillButton';
+export { Button, PillButton };
